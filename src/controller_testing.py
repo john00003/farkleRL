dc97c4fce9c5e7bf8da7c61332e7777d7fb32649
John Tyler <jetyler@ualberta.ca>
2025-07-24 08:24:34 -0600

    created play_game and play_turn functions

--- src/controller_testing.py
+++ src/controller_testing.py
@@ -6,32 +6,58 @@ import player_testing
 class FarkleController:
 
     def __init__(self, env, players, agent_player_num = 0):
-    """
-    initializes the FarkleController class
+        """
+        initializes the FarkleController class
 
-    Parameters
-    ----------
-    env: FarkleEnv
-        the gymnasium environment that will be played
-    players: array-like
-        a list of player objects
-    agent_player_num: int
-        the index in the players of the agent that is training
-    """
+        Parameters
+        ----------
+        env: FarkleEnv
+            the gymnasium environment that will be played
+        players: array-like
+            a list of player objects
+        agent_player_num: int
+            the index in the players of the agent that is training
+        """
         assert agent_player_num < len(players)
         self._env = env
         self.players = players
         self.agent_player_num = agent_player_num # TODO: can we get rid of this?
 
-    def _new_game(seed):
+    def _new_game(self,seed):
         return self._env.reset(seed)
-        
-    def play(seed):
-        observation, info = self._new_game(seed)
-        while info.winner == -1:
-            
 
-        pass
+    def play_turn(self, player, observation, info):
+        """
+        play one player's turn, moving to the next player's turn 
+        """
+        # if info.farkle: # PLAYER CAN DO ANYTHING< LET STEP HANDLE THIS
+        #     # do not update, just return
+        reward_this_turn = 0
+        player_num = observation.turn
+
+
+        while(observation.turn == player_num and not truncated and not terminated): # until we move to next player
+            lock, bank = player.play(observation) # prompt current player to play
+            action = {"lock": lock, "bank": bank}
+            observation, reward, terminated, truncated, info = env.step(action)
+            reward_this_turn += reward
+
+        player.update(reward_this_turn)
+
+        return observation, reward, terminated, truncated, info
+        
+    def play_game(self, seed):
+        """
+        play an entire game
+        """
+        observation, info = self._new_game(seed)
+        while info.winner == -1: # while game is not over TODO: consider truncated or terminated?
+            current_player = observation.turn
+            observation, reward, terminated, truncated, info = play_turn(self.players[observation.turn], observation, info)
+
+        print(f"Winner is player {info.winner}!")
+        return
+
 
 if __name__ == "__main__":
     print("what")
