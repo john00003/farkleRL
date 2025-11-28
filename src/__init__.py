88c87054ea691791a2b9030fc12e38e6d0622f30
John Tyler <jetyler@ualberta.ca>
2025-11-28 09:20:48 -0700

    reflected renaming in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from .farkle import FarkleEnv
 from .player import Player, RLAgent, RandomPlayer, SinglePlayerRLAgent, ManualPlayer
 from .controller import FarkleController
-from .training import FarkleTrainer
+from .training import TrainingRunner
 from .wrapper import FarkleEnvSinglePlayerWrapper
+from .dqn_agent import DQNAgent
 
